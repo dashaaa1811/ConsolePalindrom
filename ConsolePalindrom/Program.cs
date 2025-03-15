8bce88aa20892a646da99403ce4cf884c74bd39d
Dasha <darabrueva65@gmail.com>
2025-03-15 12:30:17 +0300

    добавила комментарий к задаче

--- ConsolePalindrom/Program.cs
+++ ConsolePalindrom/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//проверка, является ли введённая строка палиндромом
 namespace ConsolePalindrom
 {
     internal class Program
