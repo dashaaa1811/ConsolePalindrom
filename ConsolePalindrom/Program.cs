3bcb27a6f7e7db3a2686d024410db0b453620fb3
Dasha <darabrueva65@gmail.com>
2025-03-15 12:28:27 +0300

    Добавьте файлы проекта.

---
+++ ConsolePalindrom/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePalindrom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите строку");
+            string str= Console.ReadLine();
+            if (ItsPalindrome(str))
+            {
+                Console.WriteLine("строка является палиндромом");
+            }
+            else
+            {
+                Console.WriteLine("строка не является палиндромом");
+            }
+            Console.ReadKey();
+        }
+
+        static bool ItsPalindrome(string str)
+        {
+            int left = 0;
+            int right = str.Length - 1;
+            while (left<right)
+            {
+                if (str[left] != str[right])
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+    }
+}
